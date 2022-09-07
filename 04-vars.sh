4bb60395ade117f858dfbdd741eb9bfdbea6aab0
Shravan <shravan.kumbar@gmail.com>
2022-09-07 17:39:51 +0530

    Added varshes

---
+++ 04-vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+a=10
+# a is 10 and is integer
+
+b=abc
+# abc is string
+
+#### No datatypes in bash scripting.Everthing is a string by default:
+
+echo value of a is : $a
+ech0 $a
+echo "${b}"
