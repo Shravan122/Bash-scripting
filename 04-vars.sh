1cc1bf25efcc769038b858bb38b4aae2039ff567
Shravan <shravan.kumbar@gmail.com>
2022-09-07 18:32:33 +0530

    Added Loggedin users

--- 04-vars.sh
+++ 04-vars.sh
@@ -9,6 +9,7 @@ b=abc
 d=150 
 
 Date=$(Date +%F)
+LOGGEDIN_USERS=$(who | wc -l)
 
 
 #### No datatypes in bash scripting.Everthing is a string by default:
@@ -19,6 +20,8 @@ echo "${b}"
 echo "${d}"
 
 echo value of d is : $d
+echo "Numbaer of LoggedIn Users are: $LOGGEDIN_USERS"
+  
 
 DATE=2022-09-05
 echo "good morning, todays date is $DATE"
