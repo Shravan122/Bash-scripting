ef9f990bd7bb433351a0cca1242376441766a129
Shravan <shravan.kumbar@gmail.com>
2022-09-07 18:51:52 +0530

    Added special variables

---
+++ 05-special-vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "script name that you are running is $0"
+
+
+a=10
+b=$1
+c=$2
+d=$3
+
+echo value of a is = $a
+echo value of b is = $1
+echo value of c is = $2
+echo value of d is = $3
+
+# sh scriptname.sh 100 200 300
+# sh scriptname.sh $1  $2  $3
