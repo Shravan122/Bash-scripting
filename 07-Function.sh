2c858202ca64396b4555005661244535243029fa
Shravan <shravan.kumbar@gmail.com>
2022-09-08 15:51:42 +0530

    corrected stat Function

--- 07-Function.sh
+++ 07-Function.sh
@@ -22,9 +22,9 @@ sample
 
 
 stat() {
-    echo  "Load average on the system from last 1 min is : $(uptime | awk -F : '{print $NF}') | awk -F , {print $1}')
-    echo " Number of users signed in is : $(who|wc -l)'
-    echo Stat Function is completed bye"
+    echo  "Load average on the system from last 1 min is : $(uptime | awk -F : '{print $NF}') | awk -F , {print $1}')"
+    echo "Number of users signed in is : $(who|wc -l)"
+    echo  "Stat Function is completed bye"
 
 }
 
