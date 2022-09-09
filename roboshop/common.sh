f24d3073c45f2cc0c79ae6808b0f3ca369d99ddc
Shravan <shravan.kumbar@gmail.com>
2022-09-09 17:14:10 +0530

    Added source

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
 
-echo "I am installing catalogue"
-
-
+# validating whether the  executing user is root or not
 ID=$(id -u)
 if [ $ID -ne 0 ]; then 
     echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
     exit 1
-fi 
+fi
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "Iam installing mongodb"
+
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then 
+    echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
+    exit 1
+fi 
