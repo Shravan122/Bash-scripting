b8e3e128469f2c3e67a5eec37ab0810ba1bb8e55
Shravan <shravan.kumbar@gmail.com>
2022-09-11 11:33:38 +0530

    Added tmp log file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,7 +4,7 @@ set -e
 
 source components/common.sh
 
- yum install nginx -y  >> /tmp/frontend.log
+ yum install nginx -y   >> /tmp/frontend.log
  systemctl enable nginx
  systemctl start nginx 
  curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
