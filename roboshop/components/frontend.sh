7a17ab5603a3501cf40653959403067eae99f4e3
Shravan <shravan.kumbar@gmail.com>
2022-09-11 11:44:37 +0530

    corrected

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,14 +4,14 @@
 
 source components/common.sh
 
- yum install nginx -y   >> /tmp/frontend.log
+ yum install nginx -y   
  systemctl enable nginx
  systemctl start nginx 
  curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
   cd /usr/share/nginx/html
   rm -rf *
-  unzip /tmp/frontend.zip  >> /tmp/frontend.log
+  unzip /tmp/frontend.zip  
   mv frontend-main/* .
   mv static/* .
   rm -rf frontend-main README.md
