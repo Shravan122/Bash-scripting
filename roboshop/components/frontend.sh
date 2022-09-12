f6b6a67939f0ebc52fa3a5372b4a049c93213be5
Shravan <shravan.kumbar@gmail.com>
2022-09-12 13:20:27 +0530

    Added if and else

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,17 +5,22 @@ source components/common.sh
 
  echo "Installing nginx: "
  yum install nginx -y  >>/tmp/frontend.log 
-   echo -e "\e[32m Success \e[0m"
-    if [ $? -eq 0 ] ; then 
-   else 
+   if [ $? -eq 0 ] ; then 
+     echo -e "\e[32m Success \e[0m"
+  else 
      echo -e "\e[31m Failure.Look for the logs \e[0m" 
-   fi     
-
+   fi   
 
  systemctl enable nginx 
 
  echo "starting nginx: "
- systemctl start nginx
+ systemctl start  
+  if [ $? -eq 0 ] ; then 
+    echo -e "\e[32m Success \e[0m" 
+  else 
+    echo -e "\e[31m Failure.Look for the logs \e[0m" 
+  fi
+    
  curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
  cd /usr/share/nginx/html
