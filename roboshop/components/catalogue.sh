96f3f7dd7460862b32e3a66821327acbaa22bdaa
Shravan <shravan.kumbar@gmail.com>
2022-09-12 22:17:07 +0530

    Added catalog

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -34,7 +34,17 @@ FUSER=roboshop
 
     echo -n "Installing $COMPONENT Dependencies:" 
     npm install  &>>/tmp/${COMPONENT}.log
-    stat $?
+    stat $? 
+
+    echo -n "Configuring the Systemd file:"
+     sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal' /home/${FUSER}/${COMPONENT}/systemd.service
+     mv /home/${FUSER}/${COMPONENT}/systemd.service 
+     stat $?
+      
+      echo -n "Starting the service:"
+        systemctl daemon-reload    >>/tmp/${COMPONENT}.log 
+        systemctl enable catalogue >>/tmp/${COMPONENT}.log 
+        systemctl start catalogue  >>/tmp/${COMPONENT}.log 
 
 
 
