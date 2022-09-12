a935037d48c715a9c2b4501a938b5a2a4bac3b23
Shravan <shravan.kumbar@gmail.com>
2022-09-12 15:53:44 +0530

    Adde mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-echo "Iam installing mongodb"
+source components/common.sh  
+COMPONENT=MongoDB
 
+echo -n "Configuring the $(COMPONENT):"
+curl -s -o /etc/yum.repos.d/$(COMPONENT).repo https://raw.githubusercontent.com/stans-robot-project/$(COMPONENT)/main/mongo.repo 
+stat $?
 
-ID=$(id -u)
-if [ $ID -ne 0 ]; then 
-    echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
-    exit 1
-fi 
+echo -n "Installing $(COMPONENT):" 
+yum install -y mongodb-org
+stat $? 
+
+echo "Updating the $COMPONENT config:"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
+stat $?
+
+echo -n "Start the $COMPONENT service:" 
+systemctl enable mongod >> /tmp/$(COMPONENT).log
+systemctl start mongod 
+stat $?
