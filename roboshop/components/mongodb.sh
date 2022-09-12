99c61de6fcdbff24abb068dde521bc3cb60bd636
Shravan <shravan.kumbar@gmail.com>
2022-09-12 16:23:45 +0530

    Corrected

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 source components/common.sh  
- 
+
 COMPONENT=mongodb
 
 echo -n "Configuring the MongoDB repo:"
-curl -s -o /etc/yum.repos.d/$(COMPONENT).repo https://raw.githubusercontent.com/stans-robot-project/$(COMPONENT)/main/mongo.repo 
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo 
 stat $?
 
-echo -n "Installing $(COMPONENT):" 
+echo -n "Installing ${COMPONENT}:" 
 yum install -y mongodb-org
 stat $? 
 
-echo "Updating the $COMPONENT config:"
+echo -n "Updating the $COMPONENT config:"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
 stat $?
 
 echo -n "Start the $COMPONENT service:" 
-systemctl enable mongod >> /tmp/$(COMPONENT).log
+systemctl enable mongod  >> /tmp/${COMPONENT}.log
 systemctl start mongod 
 stat $?
