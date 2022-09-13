295cd757664889de57856427ccdf0ea33d6833c3
Shravan <shravan.kumbar@gmail.com>
2022-09-13 18:57:11 +0530

    corrected

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -6,8 +6,8 @@ LOGFILE=/tmp/robot.log
 MYSQL_PASSWORD="RoboShop@1"
  
   echo -n "Configuring the $COMPONENT repo: "
-  curl -s -L -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/${COMPONENT}.repo &>> ${LOGFILE}
-  stat $? 
+  curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> ${LOGFILE}
+  stat $?
 
   echo -n "Installing $COMPONENT :"
   yum install mysql-community-server -y &>> ${LOGFILE}
