b859153f35a576f33a5dd33ddc94d08cbed44f97
Shravan <shravan.kumbar@gmail.com>
2022-09-13 16:24:39 +0530

    Added

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -34,7 +34,7 @@ source components/common.sh
  mv localhost.conf /etc/nginx/default.d/roboshop.conf 
  stat $?  
 
- for component in catalogue user ; do 
+ for component in catalogue user cart; do 
  echo -n "Updating the proxy file:" 
  sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
  stat $? 
