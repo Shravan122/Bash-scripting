b0a4b44807ac37a17bc34f84dde83181e6a95803
Shravan <shravan.kumbar@gmail.com>
2022-09-13 19:34:34 +0530

    Corrected

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -27,7 +27,7 @@ MYSQL_PASSWORD="RoboShop@1"
    DEFAULT_ROOT_PASSWORD=$(sudo grep temp /var/log/mysqld.log | head -n 1 | awk -F " " '{print $NF}')
    stat $? 
 
-    #If the exit code is non-zero then only I want to execute, if not, I would like to skip 
+    
     echo show databases | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
     if [ $? -ne 0 ]; then 
     echo -n "Reset Root Password: "
