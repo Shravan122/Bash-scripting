4c1daf79870d7a19cd308da4d91d80d51a66d16b
Shravan <shravan.kumbar@gmail.com>
2022-09-13 17:59:10 +0530

    Added

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -5,51 +5,7 @@ set -e
 source components/common.sh 
 
 COMPONENT=user
-FUSER=roboshop 
 
-  echo -n "Configure yum Remos for nodejs:"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash  >>/tmp/${COMPONENT}.log 
-  stat $?
-
-  echo -n "Installing nodejs:"
-  yum install nodejs -y  >>/tmp/${COMPONENT}.log
-   stat $? 
-
-   echo -n "Adding $FUSER user:" 
-   id ${FUSER} >>/tmp/${COMPONENT}.log || useradd ${FUSER} 
-   stat $?
-
-   echo -n "Downloading the $COMPONENT:"
-    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >>/tmp/${COMPONENT}.log
-    stat $? 
-
-    echo -n "Clean up of old $COMPONENT content:"
-    rm -rf /home/${FUSER}/${COMPONENT} >>/tmp/${COMPONENT}.log 
-    stat $?
-
-   echo -n "Extracting $COMPONENT content:"
-   cd /home/${FUSER}  >>/tmp/${COMPONENT}.log 
-   unzip -o /tmp/${COMPONENT}.zip >>/tmp/${COMPONENT}.log   && mv ${COMPONENT}-main ${COMPONENT} && cd /home/${FUSER}/${COMPONENT} >>/tmp/${COMPONENT}.log 
-   stat $?  
-
-    echo -n "Installing $COMPONENT Dependencies:" 
-    npm install  &>>/tmp/${COMPONENT}.log
-    stat $? 
-
-       echo -n "Setup Systemd file:" 
-       sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service 
-       mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-       stat $?
-      
-        echo -n "Starting the service:"
-        systemctl daemon-reload    &>>/tmp/${COMPONENT}.log 
-        systemctl enable ${COMPONENT} &>>/tmp/${COMPONENT}.log 
-        systemctl start ${COMPONENT}  &>>/tmp/${COMPONENT}.log 
-        stat $? 
-
-        
-       
-
-        
-
-    
+#Calling NODEJS function
+ NODEJS 
+ 
