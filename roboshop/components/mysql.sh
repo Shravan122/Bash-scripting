0c492cc2fe768b0bf0f96624eba8fe4cb0ba2fd3
Shravan <shravan.kumbar@gmail.com>
2022-09-13 19:09:54 +0530

    Corrected

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
+
 set -e 
 
 COMPONENT=mysql
 LOGFILE=/tmp/robot.log
 
  
-   echo -n "Configuring the $COMPONENT repo: " 
+   echo -n "Configuring the $COMPONENT repo:" 
    curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
    stat $?
 
