6e76198c6a23bdd9eb0364814d2dec65a0e7c2e1
Shravan <shravan.kumbar@gmail.com>
2022-09-13 14:19:03 +0530

    Added echo

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -14,11 +14,18 @@ echo -n "Installing $COMPONENT:"
 yum install redis-6.2.7 -y  &>> /tmp/${COMPONENT}.log
 stat $? 
 
+echo -n "Whitelisting the redis config:"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+stat $?
+
+echo -n "Starting $COMPONENT:"
+ systemctl  daemon-reload  $>> /tmp/${COMPONENT}.log
+ systemctl enable $COMPONENT  $>> /tmp/${COMPONENT}.log
+ systemctl  restart  $COMPONENT  $>> /tmp/${COMPONENT}.log
+
 
 
 
 # Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf 
 
-# systemctl enable redis
-# systemctl start redis
-# systemctl status redis -l
+
