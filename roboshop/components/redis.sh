1448fac38739c860defcf903527bf81817932cca
Shravan <shravan.kumbar@gmail.com>
2022-09-13 13:56:28 +0530

    Adde redis

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
+
+set -e 
+
+source components/common.sh 
+
+COMPONENT=redis
+
+echo -n "Configuring the $COMPONENT repo:"
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo 
+stat $?
+
+echo -n "Installing $COMPONENT:"
+yum install redis-6.2.7 -y  &>> /tmp/${COMPONENT}.log
+stat $? 
 
 
 
-ID=$(id -u)
-if [ $ID -ne 0 ]; then 
-    echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
-    exit 1
-fi 
+
+# Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf 
+
+# systemctl enable redis
+# systemctl start redis
+# systemctl status redis -l
