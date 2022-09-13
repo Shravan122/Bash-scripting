653e188d5a5a701fd3d09b9f60904882376f6fb0
Shravan <shravan.kumbar@gmail.com>
2022-09-13 18:10:38 +0530

    Added

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -5,44 +5,11 @@ set -e
 source components/common.sh 
 
 COMPONENT=cart
-FUSER=roboshop 
 
-  echo -n "Configure yum Remos for nodejs:"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash  >>/tmp/${COMPONENT}.log 
-  stat $?
+#calling NODEJS function
+NODEJS
+ 
 
-  echo -n "Installing nodejs:"
-  yum install nodejs -y  >>/tmp/${COMPONENT}.log
-   stat $? 
-
-   echo -n "Adding $FUSER user:" 
-   id ${FUSER} >>/tmp/${COMPONENT}.log || useradd ${FUSER} 
-   stat $?
-
-   echo -n "Downloading the $COMPONENT:"
-    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >>/tmp/${COMPONENT}.log
-    stat $? 
-
-    echo -n "Clean up of old $COMPONENT content:"
-    rm -rf /home/${FUSER}/${COMPONENT} >>/tmp/${COMPONENT}.log 
-    stat $?
-
-   echo -n "Extracting $COMPONENT content:"
-   cd /home/${FUSER}  >>/tmp/${COMPONENT}.log 
-   unzip -o /tmp/${COMPONENT}.zip >>/tmp/${COMPONENT}.log   && mv ${COMPONENT}-main ${COMPONENT} && cd /home/${FUSER}/${COMPONENT} >>/tmp/${COMPONENT}.log 
-   stat $?  
-
-    echo -n "Installing $COMPONENT Dependencies:" 
-    npm install  &>>/tmp/${COMPONENT}.log
-    stat $? 
-
-      echo -n "Setup Systemd file:" 
-       sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service 
-       mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-       stat $?
+   
       
-      echo -n "Starting the service:"
-        systemctl daemon-reload    &>>/tmp/${COMPONENT}.log 
-        systemctl enable ${COMPONENT} &>>/tmp/${COMPONENT}.log 
-        systemctl start ${COMPONENT}  &>>/tmp/${COMPONENT}.log 
-        stat $?
+     
--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -7,5 +7,5 @@ source components/common.sh
 COMPONENT=user
 
 #Calling NODEJS function
- NODEJS 
+NODEJS 
  
