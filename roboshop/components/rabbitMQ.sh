2a3b76a61513176bb93fdce3d6d0727005dfec26
Shravan <shravan.kumbar@gmail.com>
2022-09-14 17:36:43 +0530

    Changed conf and install

--- roboshop/components/rabbitMQ.sh
+++ roboshop/components/rabbitMQ.sh
@@ -5,14 +5,12 @@ source components/common.sh
 COMPONENT=rabbitmq 
 LOGFILE=/tmp/robot.log
 
-echo -n "Installing Erlang dependency:"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>${LOGFILE}
+echo -n "Configuring and Installing dependency:" 
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> ${LOGFILE} 
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>> ${LOGFILE}
 stat $?
  
- echo -n "Configuring the YUM repo for $COMPONENT:"
- curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOGFILE}
- stat $? 
 
  echo -n "Installing RabbitMQ:"
- yum install rabbitmq-server -y &>>${LOGFILE}
+ yum install rabbitmq-server -y &>> ${LOGFILE}
  stat $?
