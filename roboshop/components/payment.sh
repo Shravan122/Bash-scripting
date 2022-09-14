0fa3b6e1127b27c576a824c34e17bcf6d6b92330
Shravan <shravan.kumbar@gmail.com>
2022-09-14 18:56:42 +0530

    Added

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -4,5 +4,14 @@ source components/common.sh
 
 COMPONENT=payment
 
-echo -n "Installing "
+echo -n "Installing Python: " 
+yum install python36 gcc python3-devel -y  &>> ${LOGFILE}
+stat $?
 
+USER_SETUP 
+
+DOWNLOAD_AND_EXTRACT 
+
+cd /home/${FUSER}/${COMPONENT} 
+pip3 install -r requirements.txt 
+stat $?
