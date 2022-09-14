7b42e4a739ed70c7f6f29204875fbeba1a8dacfd
Shravan <shravan.kumbar@gmail.com>
2022-09-14 16:13:07 +0530

    Added rabbitmq

--- roboshop/components/rabbitMQ.sh
+++ roboshop/components/rabbitMQ.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
- source components/common.sh 
+  source components/common.sh 
 
- COMPONENT=rabbitmq 
+  COMPONENT=rabbitmq 
 
   echo -n "Installing Erlang dependecy:" 
   yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
