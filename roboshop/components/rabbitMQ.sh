f5287c1350e81a7898ad0770c697e3244ed90f3b
Shravan <shravan.kumbar@gmail.com>
2022-09-14 16:54:45 +0530

    Added repo

--- roboshop/components/rabbitMQ.sh
+++ roboshop/components/rabbitMQ.sh
@@ -7,4 +7,7 @@ COMPONENT=rabbitmq
 echo -n "Installing Erlang dependency:"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y 
 stat $?
-
+ 
+ echo -n "Configuring the YUM repo for $COMPONENT:"
+ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+ stat $?
