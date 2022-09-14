f3bf77c81967ae12366b6a2460b88a5a8c58278d
Shravan <shravan.kumbar@gmail.com>
2022-09-14 19:42:41 +0530

    Changed

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -16,8 +16,8 @@ cd /home/${FUSER}/${COMPONENT}
 pip3 install -r requirements.txt  &>> ${LOGFILE}
 stat $? 
 
-USER_ID=$id(id -u roboshop)
-GROUP_ID=$id(id -u roboshop) 
+USER_ID=$id{id -u roboshop}
+GROUP_ID=$id{id -u roboshop} 
 
 echo -n "Updating the $COMPONENT.ini file:"
 sed -i -e "/^uid/ c uid=${USERID}" -e "/gid/c gid=${GROUPID}" payment.ini 
