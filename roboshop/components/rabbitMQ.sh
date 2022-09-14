dadf5024c3215e57889d5cab9007fca6690719ab
Shravan <shravan.kumbar@gmail.com>
2022-09-14 16:02:06 +0530

    Added rabbitmq

--- roboshop/components/rabbitMQ.sh
+++ roboshop/components/rabbitMQ.sh
@@ -1,8 +1,23 @@
+#!/bin/bash
 
+ source components/common.sh 
 
+ COMPONENT=rabbitmq 
 
-ID=$(id -u)
-if [ $ID -ne 0 ]; then 
-    echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
-    exit 1
-fi 
+  echo -n "Installing Erlang dependecy:" 
+  yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+  stat $? 
+
+  echo -n "Configuring the YUm for $COMPONENT:" 
+  curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+  stat $?
+
+  echo -n "Installing RabbitMQ:" 
+  yum install rabbitmq-server -y 
+  stat $? 
+
+  echo -n "Starting $COMPONENT:" 
+  systemctl enable rabbitmq-server 
+  systemctl start rabbitmq-server
+  stat $?
+
