8fbe94b6738a9836b9cb33f4cec871c74a177b74
Shravan <shravan.kumbar@gmail.com>
2022-09-17 17:39:40 +0530

    Added

--- roboshop/launch.ec2.sh
+++ roboshop/launch.ec2.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/bash 
+
+COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ' .Images[].ImageId'|sed 's/"//g')
-echo "The AMI which we are using is $AMI_ID"
+SGID="sg-092136d71863d6090"
+echo "The AMI which we are using is $AMI_ID" 
+
+
+aws ec2 run-instances --image-id ${AMI_ID} ---instance-type t3.micro   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  --security-group-ids ${SGID} 
