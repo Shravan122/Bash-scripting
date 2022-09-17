cb11c055836db60d9d6dab8032574d973e6e4426
Shravan <shravan.kumbar@gmail.com>
2022-09-17 19:47:11 +0530

    Added

--- roboshop/launch.ec2.sh
+++ roboshop/launch.ec2.sh
@@ -6,13 +6,19 @@ if [ -z  "$1" ] ; then
     exit 1
 fi
 
-COMPONENT=$1
+COMPONENT=$1 
+ZONEID="Z088865137EDV0W245DON"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ' .Images[].ImageId'|sed 's/"//g')
 SGID="sg-092136d71863d6090"
 echo "The AMI which we are using is $AMI_ID" 
 
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | jq 
+PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
+echo "Private IP of the created machine is $PRIVATE_IP"
 echo "Spot Instance $COMPONENT is ready:"
+echo "Creating Route53 Record . . . . :" 
+
+ sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" r53.json  >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch file:///tmp/record.json | jq 
