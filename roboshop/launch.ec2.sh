435516bee820c0e62dd719b7c23445c1199eab33
Shravan <shravan.kumbar@gmail.com>
2022-09-17 18:12:31 +0530

    Added

--- roboshop/launch.ec2.sh
+++ roboshop/launch.ec2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 
+# If $1 is empty or $1 is not supplied, then I want to mark it as failure.
+if [ -z  "$1" ] ; then 
+    echo -e "\e[31m Machine Name Is Missing \e[0m"
+    exit 1
+fi
+
 COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ' .Images[].ImageId'|sed 's/"//g')
