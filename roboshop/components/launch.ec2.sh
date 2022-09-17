ded79903a465505492612ff7a3718e8c85baef78
Shravan <shravan.kumbar@gmail.com>
2022-09-17 16:08:08 +0530

    Corrected

--- roboshop/components/launch.ec2.sh
+++ roboshop/components/launch.ec2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ' .Images[].ImageId'|sed 's/"//g')
-echo "The AMi which we are using is $AMI_ID:"
+echo "The AMI which we are using is $AMI_ID"
